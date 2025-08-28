032c27c9fc27ad455415fbf1b126a641d8a10713
chaoscoder456 <yashasvi.akhilesham@dpsnashik.in>
2025-08-28 09:36:13 +0530

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenSwitchUp, function (sprite, location) {
-    tiles.setCurrentTilemap(tilemap`level2`)
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
+    game.splash("")
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.greenSwitchUp, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level1`)
+})
+let location = 0
 let myCorg = corgio.create(SpriteKind.Player)
 myCorg.horizontalMovement()
 myCorg.verticalMovement()
 myCorg.updateSprite()
-tiles.setCurrentTilemap(tilemap`level1`)
+scene.setBackgroundColor(8)
+scene.cameraFollowSprite(myCorg)
+tiles.setCurrentTilemap(tileUtil.createSmallMap(tilemap`level1`))
+tiles.placeOnTile(myCorg, tiles.getTileLocation(0, 9))
+location = location
